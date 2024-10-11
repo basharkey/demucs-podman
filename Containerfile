4b2872b342ad92b85a63fb940292b3eca30e8209
basharkey <15859442+basharkey@users.noreply.github.com>
2024-10-10 23:02:39 -0400

    containerfile and initial script

---
+++ Containerfile
@@ -0,0 +1,24 @@
+FROM registry.hub.docker.com/library/debian:12
+
+RUN apt-get update && \
+    DEBIAN_FRONTEND=noninteractive \
+    apt-get install -y --no-install-recommends \
+    ffmpeg \
+    python3 \
+    python3-pip
+
+RUN python3 -m pip install --no-cache-dir --break-system-packages -U demucs
+
+RUN mkdir /input
+RUN mkdir /output
+
+# Run once to pull and store default model
+RUN mkdir /models
+ENV TORCH_HOME=/models
+COPY test.mp3 test.mp3
+RUN demucs -d cpu test.mp3 
+RUN rm -rf separated
+RUN rm -f test.mp3
+
+ENTRYPOINT ["/bin/bash", "-c"]
+CMD ["demucs", "/input/*", "--out", "/output", "-d", "cpu"]
---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+
+album_dir=$1
+stem_type=$2
+
+if ! ([[ -n "$album_dir" ]] && [[ -d "$album_dir" ]] && [[ "$stem_type" =~ ^(vocals|drums|bass|other)$ ]]); then
+    echo "Removes the specified stem from songs in an album"
+    echo
+    echo "Usage: $0 <album dir> (vocals|drums|bass|other)"
+    echo "e.g. $0 ~/Music/artists/Good \\Game/Good\\ Game\\ -\\ Get\\ Good/ vocals"
+    echo
+    exit 1
+fi
+
+artist_name="$(basename "$(dirname "$album_dir")")"
+album_name="$(basename "$album_dir")"
+stem_dir=~/Music/stems/"$artist_name"/"$album_name"
+
+echo "$stem_dir"
+mkdir -p "$stem_dir"
+podman run --rm -it -v "$album_dir":/input -v "$stem_dir":/output demucs:latest \
+       "demucs /input/* --out /output -d cpu --mp3 --two-stems "$stem_type""
+
+cp -r "$stem_dir"/htdemucs/* "$stem_dir"
+rm -r "$stem_dir"/htdemucs
+
